d04e6ef905d3ac3fa8a16e44427c3b13854ffd44
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2022-09-08 15:15:22 +0200

    AddModal

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Components/Header'
-import CategoryTitle from './Components/CategoryTitle'
-
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import CategoryTitle from "./Components/CategoryTitle";
+import AddModal from "./Components/AddModal";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false) 
   return (
     <div className="App">
-      <Header/>
-      <CategoryTitle noAdd={true} small={false} category="albums" title="Albums" image="albums"/>
-      <CategoryTitle noAdd={true} small={true} category="images" title="Images" image="images"/>
-      <CategoryTitle noAdd={false} small={true} category="favorites" title="Favorites" image="favorites"/>
+      {openModal ? <AddModal show={setOpenModal}/>: <div />}
+      
+      <div>
+        <Header />
 
-
+        <CategoryTitle
+          noAdd={true}
+          small={false}
+          category="albums"
+          title="Albums"
+          image="albums"
+        />
+        <CategoryTitle
+          noAdd={true}
+          small={true}
+          category="images"
+          title="Images"
+          image="images"
+        />
+        <CategoryTitle
+          noAdd={false}
+          small={true}
+          category="favorites"
+          title="Favorites"
+          image="favorites"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
