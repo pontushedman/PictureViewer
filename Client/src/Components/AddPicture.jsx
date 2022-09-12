ab37e7ad1c5cdfa128aa45a907454f65e5059fa6
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-09-12 17:27:11 +0200

    Commented the AddPicture Component

--- Client/src/Components/AddPicture.jsx
+++ Client/src/Components/AddPicture.jsx
@@ -2,6 +2,8 @@ import style from "./Styles/AddPicture.module.css";
 import React from "react";
 import { useState, useEffect } from "react";
 
+
+//Function to populate Localstorage
 function setToLocalStorage(key, value) {
   try {
     localStorage.setItem(key, value);
@@ -11,9 +13,13 @@ function setToLocalStorage(key, value) {
   }
 }
 
+//This component is being used to fill the modal when modalstate === addPicturess
 function AddPicture() {
+  //State used to rerender component
   const [selectedFiles, setSelectedFiles] = useState([])
   
+
+  //Component that shows the "Add Picture" area and handles all the adding
   function FormAdd() {
     return (
       <div className={style.largeAdd}>
@@ -22,29 +28,43 @@ function AddPicture() {
         </div>
         <div className={style.chooseDiv}>
           <button className={style.uploadButton}></button>
+
+          {/*This inputs onChange triggers when you have chosen an image, 
+          //That image is being used as input in the callback function, (image) in this case
+          //We read that image in the filereader and converts it to a dataURL(file in textformat???).
+          //We store the file in a canvas that we resize, we then access the data in the canvas and converts it to a dataURL
+          //That is how we save two pictures in two different sizes.*/}
           <input 
-            onChange={(e) => {
-              const file = e.target.files[0]   
+            onChange={(image) => {
+              const file = image.target.files[0]   
               var reader = new FileReader()
                 reader.readAsDataURL(file)
                 reader.addEventListener("load", () => {
+                 
+                  //Create Image
                   const img = document.createElement("img")
-                  var resizedImage = ""
+                  //Convert image
                   img.onload = (() => {
                      const canvas = document.createElement("canvas")
                      const ctx = canvas.getContext("2d")
                     
                      const convertedHeight = img.height * 0.5
                      const convertedWidth = img.width * 0.5
+                     
                      canvas.width = convertedWidth
                      canvas.height = convertedHeight
                      
-
                      ctx.drawImage(img, 0, 0, convertedWidth , convertedHeight)
                      const resizedImage = canvas.toDataURL(file.type);
+
+                     //Store images and empty properties in Localstorage.
                      setToLocalStorage(file.size, JSON.stringify({hires_image: reader.result, lowres_image: resizedImage, title: "", comment: "", albums: []}))
+                     //Rerender component to reflect changes in Localstorage.
                      setSelectedFiles(localStorage.length)
                     })
+
+                  //Populate image element with original image.
+                  //This is to trigger the img.onload function.  
                   img.src=reader.result
               })           
             }}  
@@ -54,20 +74,15 @@ function AddPicture() {
       </div>)
   }
 
-  function resizeImage() {
-
-  }
-
-
+  //Loop through localstorage and show what's returned in input and image tags
   function ChosenPictures() {
     console.log("Localstorage length" + localStorage.length)
-    let a = []
+    let pictureContainer = []
     for (let index = 0; index < localStorage.length; index++) {
       let key = localStorage.key(index)
       let value = JSON.parse(localStorage.getItem(key))
-      //console.log("key " + key + index)
-      
-      a.push (
+
+      pictureContainer.push (
         <div className={style.chosenPictureContainer}>
           <div className={style.chosenPictureBoundary}>
             <img className={style.chosenPicture} src={value.hires_image} />
@@ -81,17 +96,25 @@ function AddPicture() {
         </div>
       )
     }
-    return a
+    return pictureContainer
   }
 
+  //Push the objects in localstorage to a singleobjects that's supposed to be sent in a POST request
   function logLocalStorage() {
+    let images = []
     for (let index = 0; index < localStorage.length; index++) {
       let key = localStorage.key(index)
       let value = JSON.parse(localStorage.getItem(key))
-      console.log(value)
+      images.push(value)
     }
+    const storageObjects = {...images}
+    console.log(storageObjects)
   }
 
+  //Prevent submitting a form for now and log the objects in localstorage
+  //We have to use useEffect here to access the submit button after render.
+  
+  //**When code is inside the useEffect hook, it only executes after component is rendered**
   useEffect(() => {
     const button = document.getElementById("submit")
     button.addEventListener("click", ((event) => {
--- Client/src/Components/Modal.jsx
+++ Client/src/Components/Modal.jsx
@@ -18,20 +18,3 @@ function Modal(props) {
 }
 
 export default Modal;
-
-
-  // return (<div className={style.modalBackground}>
-  //     <div className={style.modalContainer}>
-  //         <button> X </button>
-  //         <div className={style.title}>
-  //             <h1>Are you sure want to continue</h1>
-  //         </div>
-  //         <div className={style.body}>
-  //             <p>The next page is awsome! You shlod move forward</p>
-  //         </div>
-  //         <div className={style.fotter}>
-  //             <button onClick={() => props.showModal(false)}> Cancel </button>
-  //             <button> Contiune </button>
-  //         </div>
-  //     </div>
-  // </div>)
