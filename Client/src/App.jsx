40f7c0bb74230e24ad63a18dcef40350179ac0ef
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-09-12 07:55:26 +0200

    Added routing to Albums and Pictures

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+
 import "./App.css";
 import Header from "./Components/Header";
 import Modal from "./Components/Modal";
 import Footer from "./Components/Footer"
 import FrontPage from "./Pages/FrontPage";
+import AlbumsPage from "./Pages/AlbumsPage";
+import PicturesPage from "./Pages/PicturesPage";
+
 
 function App() {
   const [openModal, setOpenModal] = useState(false)
 
   console.log("STATE IS " + openModal)
-  
+
   return (
-    <div className="App">
-      {openModal ? <Modal show={openModal} showModal={setOpenModal} /> : <div/>}
-      <Header />
-      <FrontPage setOpenModal={setOpenModal}/>
-      <Footer/>
-    </div>
+      <div className="App">
+        {openModal ? <Modal show={openModal} showModal={setOpenModal} /> : <div/>}
+        <Header />
+          <Routes>
+            <Route path="/" element={<FrontPage setOpenModal={setOpenModal} />} />
+            <Route path="/Albums" element={<AlbumsPage/>} />
+            <Route path="/Pictures" element={<PicturesPage/>} />
+          </Routes>
+        <Footer />
+      </div>
   );
 }
 
