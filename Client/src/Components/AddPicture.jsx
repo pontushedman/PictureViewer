76f2db7a80ef946c9da85ddaf13cca273c7305f1
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-09-12 16:50:21 +0200

    Code is now storing each uploaded image as a separate object(stringified) in localstorage. We are also storing a lowres image converted after selecting image automatically

--- Client/src/Components/AddPicture.jsx
+++ Client/src/Components/AddPicture.jsx
@@ -12,17 +12,7 @@ function setToLocalStorage(key, value) {
 }
 
 function AddPicture() {
-  const [chosenImages, setChosenImages] = useState([<FormAdd/>]);
   const [selectedFiles, setSelectedFiles] = useState([])
-
-  useEffect(() => {
-    if(selectedFiles.length > 0) {
-      selectedFiles.map(x => {
-        var s = localStorage.getItem(x)
-        console.log("Item from storage " + s)
-      })
-    }
-  }), []
   
   function FormAdd() {
     return (
@@ -34,48 +24,82 @@ function AddPicture() {
           <button className={style.uploadButton}></button>
           <input 
             onChange={(e) => {
-              const file = e.target.files[0]
-              
+              const file = e.target.files[0]   
               var reader = new FileReader()
-              reader.readAsDataURL(file)
-              reader.addEventListener("load", () => {
-                setToLocalStorage(file.size, reader.result)
-                setSelectedFiles([file.size, ...selectedFiles])
+                reader.readAsDataURL(file)
+                reader.addEventListener("load", () => {
+                  const img = document.createElement("img")
+                  var resizedImage = ""
+                  img.onload = (() => {
+                     const canvas = document.createElement("canvas")
+                     const ctx = canvas.getContext("2d")
+                    
+                     const convertedHeight = img.height * 0.5
+                     const convertedWidth = img.width * 0.5
+                     canvas.width = convertedWidth
+                     canvas.height = convertedHeight
+                     
+
+                     ctx.drawImage(img, 0, 0, convertedWidth , convertedHeight)
+                     const resizedImage = canvas.toDataURL(file.type);
+                     setToLocalStorage(file.size, JSON.stringify({hires_image: reader.result, lowres_image: resizedImage, title: "", comment: "", albums: []}))
+                     setSelectedFiles(localStorage.length)
+                    })
+                  img.src=reader.result
               })           
             }}  
             className={style.chooseFile} type="file" name="picture"
           />
         </div>
       </div>)
-    
+  }
+
+  function resizeImage() {
+
   }
 
 
   function ChosenPictures() {
-    var s = localStorage.getItem("apa")
     console.log("Localstorage length" + localStorage.length)
     let a = []
     for (let index = 0; index < localStorage.length; index++) {
       let key = localStorage.key(index)
-      console.log("key " + key + index)
+      let value = JSON.parse(localStorage.getItem(key))
+      //console.log("key " + key + index)
       
       a.push (
         <div className={style.chosenPictureContainer}>
           <div className={style.chosenPictureBoundary}>
-            <img className={style.chosenPicture} src={localStorage.getItem(key)} />
+            <img className={style.chosenPicture} src={value.hires_image} />
           </div>
-          <input className={style.choice}type="text" placeholder="Title" name="Title" />
-          <input className={style.choice} type="text" placeholder="Comment" name="Comment" />
-          <select className={style.choice}>
+          <input className={style.choice} name={key + "_Title"} type="text" placeholder="Title"/>
+          <input className={style.choice} name={key + "_Comment"} type="text" placeholder="Comment"/>
+          <select name={key + "_Album"} className={style.choice}>
             <option>Album 1</option>
             <option>Album 2</option>
           </select>
         </div>
       )
     }
-    return a   
+    return a
   }
 
+  function logLocalStorage() {
+    for (let index = 0; index < localStorage.length; index++) {
+      let key = localStorage.key(index)
+      let value = JSON.parse(localStorage.getItem(key))
+      console.log(value)
+    }
+  }
+
+  useEffect(() => {
+    const button = document.getElementById("submit")
+    button.addEventListener("click", ((event) => {
+      event.preventDefault()
+      logLocalStorage()
+    }))
+  }, [])
+  
 
   return (
     <div className={style.container}>
@@ -84,12 +108,14 @@ function AddPicture() {
         <p className={style.title}>Add Pictures</p>
         <FormAdd/>
       </div>
-        <form className={style.formx}>
+
+        <form method="post" className={style.formx}>
           <div className={style.collection}>
             <ChosenPictures/>
           </div>
-          <button className={style.choosebtn}>Upload</button>
+          <button type="submit" id="submit" className={style.choosebtn}>Upload</button>
         </form>
+
       </div>
     </div>
   );
