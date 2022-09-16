4507b53e110cf472dcf894681b7b124f6a3e443a
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-09-16 09:07:17 +0200

    Added functionlaity to disable modal by clicking the blurred background

--- Client/src/Components/Modal.jsx
+++ Client/src/Components/Modal.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import AddPicture from "./AddPicture";
 import style from "./Styles/Modal.module.css";
 import ShowImage from "./ShowImage";
+import { useEffect } from "react";
 
 function Modal(props) {
+
+  useEffect(() => {
+    const background = document.getElementById("back")
+    background.addEventListener("click", (e) => {
+      const b = e.target.getAttribute("id")
+      if(b === "back") {
+        props.showModal(false)
+      } else {
+        console.log("apa")
+      }
+    })
+  })
+
   const p = {...props}
   return (
-    <div className={style.modalBackground} onClick={() => props.showModal(false)}>
+    <div className={style.modalBackground} id="back">
       <div className={style.modalContainer}>
        {p.mode === "addimages" ? <AddPicture/> : null}
        {p.mode === "image" ? <ShowImage id={props.data.image.id}/> : null}
