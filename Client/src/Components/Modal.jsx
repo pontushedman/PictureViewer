89ccd4b34e2d7d691e9e9061cf3721ab8f9a80ae
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-09-19 15:44:11 +0200

    Changed evenlistener on modal to onClick

--- Client/src/Components/Modal.jsx
+++ Client/src/Components/Modal.jsx
@@ -3,24 +3,20 @@ import AddPicture from "./AddPicture";
 import style from "./Styles/Modal.module.css";
 import ShowImage from "./ShowImage";
 import ShowAlbum from "./ShowAlbum";
-import { useEffect } from "react";
 import AddAlbum from "./AddAlbum";
 
-function Modal(props) {
-  useEffect(() => {
-    const background = document.getElementById("back")
-    background.addEventListener("mousedown", (e) => {
-      const b = e.target.getAttribute("id")
-      if (b === "back") {
-        props.showModal(false)
-      } else {
-        console.log("apa")
-      }
-    })
-  })
+function Modal(props) {  
+  function closeModal(e) {
+    const b = e.target.getAttribute("id")
+    b === "back" ? props.showModal(false) : null
+  }
 
   return (
-    <div className={style.modalBackground} id="back">
+    <div 
+      className={style.modalBackground} 
+      id="back" 
+      onClick={(e => {closeModal(e)})}
+    >
       <div className={style.modalContainer}>
         {props.mode === "addimages" ? <AddPicture /> : null}
         {props.mode === "addalbum" ? <AddAlbum /> : null}
