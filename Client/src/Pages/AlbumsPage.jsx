2a31506bf66d271bc254e4462935d45e19acbd9b
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2022-09-22 15:32:58 +0200

    test push again same appearances

--- Client/src/Pages/AlbumsPage.jsx
+++ Client/src/Pages/AlbumsPage.jsx
@@ -2,9 +2,15 @@ import JSONContext from '../Store/JSONContext'
 import { useContext, } from 'react'
 import styles from './Styles/AlbumsPage.module.css'
 
-function AlbumsPage() {
+function AlbumsPage(props) {
 
   const JSONCtx = useContext(JSONContext)
+
+  function openAlbum(e) {
+    const albumId = e.target.dataset.id
+    const obj = { show: true, mode: "album", id: albumId }
+    props.showModal(obj)
+  }
   console.log("Albumspage rendered")
   return (
     (
@@ -14,12 +20,24 @@ function AlbumsPage() {
           <p className={styles.title}>All Albums</p>
         </div>
         <div className={styles.Albums}>
-          {JSONCtx.AlbumsList.map(x =>
-            <div className={styles.Album}>
-              <img className={styles.AlbumHeaderImage} src={"http://localhost:3000/" + x.headerImage} />
-              <p className={styles.AlbumTitle}>{x.title}</p>
-              <p className={styles.AlbumImageCount}>{x.pictures.length} Pictures</p>
-            </div>)}
+        {JSONCtx.AlbumsList.map(x =>
+          <div 
+            onClick={(e => {
+              openAlbum(e)
+            })}
+            key={x.id + 1} 
+            className={styles.Album}
+            data-id={x.id}
+            >
+            
+            <img key={x.id + 2}
+              className={styles.AlbumHeaderImage}
+              data-id={x.id}
+              src={"http://localhost:3000/" + x.headerImage}
+            />
+            <p key={x.id + 3} className={styles.AlbumTitle}>{x.title}</p>
+            <p key={x.id + 4} className={styles.AlbumImageCount}>{x.pictures.length} Pictures</p>
+          </div>)}
         </div>
       </div>
     )
