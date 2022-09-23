c16e5cbf97c629c96d354a237d1affe4b7810619
Pontus Hedman <ponhed@gmail.com>
2022-09-23 17:04:05 +0200

    Styling add removal of unused elements

--- Client/src/Components/ShowAlbum.jsx
+++ Client/src/Components/ShowAlbum.jsx
@@ -130,9 +130,6 @@ function ShowAlbum(props) {
             }
           }}
         />
-        <div className={styles.formAdd}>
-           <FormAdd/>
-        </div>
       </div>
       <div className={styles.imageTitle}>
         <p>{initialTitle}</p>
